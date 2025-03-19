3af0ced09eb6a0888ba0a6b6fff43bbcc3bdb2b8
NITHINKR06 <nithinpoojari606@gmail.com>
2025-03-19 20:44:25 +0530

    updated the router pages

--- src/Modules/Employee/router/eroute.jsx
+++ src/Modules/Employee/router/eroute.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Home from '../../Home'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import EmployeeProfile from '../pages/dashboard/EmployeeProfile'
 import SettingsPage from '../pages/settings/SettingsPage'
 import BookingStatu from '../pages/bsemployee/BookingStatu'
+import Cookies from 'js-cookie';
 
 export default function ERoute() {
+    const navigate = useNavigate('')
+
+    useEffect(() => {
+      const token = Cookies.get('token');
+      if (!token) {
+        navigate('/auth/login');
+        return;
+      }
+    }, []);
   return (
     <div>
         <Routes>
--- src/Modules/User/router/uroute.jsx
+++ src/Modules/User/router/uroute.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Home from '../../Home'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import Booking from '../pages/booking/page'
 import BookingStatu from '../pages/bookingStatus/BookingStatu'
 import Payment from '../pages/bookingStatus/[id]/Payment'
 import SameLayout from '../pages/booking/EmployeeLayout'
 import EmployeeProfile from '../pages/dashboard/EmployeeProfile'
 import SettingsPage from '../pages/settings/page'
+import Cookies from 'js-cookie';
 
 export default function URoute() {
+  const navigate = useNavigate('')
+
+    useEffect(() => {
+      const token = Cookies.get('token');
+      if (!token) {
+        navigate('/auth/login');
+        return;
+      }
+    }, []);
+
   return (
     <div>
     {/* <ResAppbar/> */}
